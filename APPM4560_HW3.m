2a1d614e021a9e10171af42fc3982b8b2acc24db
John "JJ" Dennis <143365753+JohnDennis1@users.noreply.github.com>
2026-02-02 13:37:38 -0700

    Update APPM4560_HW3.m

--- APPM4560_HW3.m
+++ APPM4560_HW3.m
@@ -21,16 +21,47 @@ num_hrs3 = num_hrs1/num_hrs2;
 %Part E
 p_50 = p^50; %p_{ij}^k represent the prob of going from state i to j after k moves
 
+%Part 5
+
 %% Problem 3
 clear;
 clc;
 
 %Part A
+%X_0 = 2 -> X_1 = 3 -> X_2 = 4 -> X_3= 5 -> X_4 = 5
+
+p25 = (2/3)^3*1;
+
+fprintf("There only exists one path where X_4 = 5 given X_0 = 2. Calulating the probability comes to (2/3)^3 or %.4f \n", p25);
+
+%Part B
 p = [1 0 0 0 0;
     1/3 0 2/3 0 0;
     0 1/3 0 2/3 0;
     0 0 1/3 0 2/3;
     0 0 0 0 1];
 
-%Part B
+%Part C
 p_4 = p^4;
+
+p4_25 = p_4(2,5);
+
+fprintf("The probability X_4 = 5 given X_0 = 2 is equal to %.4f by looking at the p^4(2,5) entry", p4_25);
+
+%% Problem 4
+clear;
+clc;
+
+p = [0.5 0.5 0 0 0 0;
+    0 0.5 0.5 0 0 0;
+    (1/3) 0 (1/3) (1/3) 0 0;
+    0 0 0 0.5 0.5 0;
+    0 0 0 0 0 1;
+    0 0 0 0 1 0];
+
+%Part A
+
+%Part B
+p_5 = p^5;
+
+p_5(1,5)
